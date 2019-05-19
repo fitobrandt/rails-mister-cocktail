c046e8b8244b68645a2478e5c2ff604ea355735d
Fito Brandt <fitodevelopment@gmail.com>
2019-05-19 18:13:57 -0400

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails , only: [:index, :new, :create, :show ] do
     # to create a new dose I need cocktail id => nested
